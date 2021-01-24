e9cc21889c0b334c56f56a636bb7a3f8f80b4517
heruns <hhccrr@foxmail.com>
2021-01-24 22:52:56 +0800

    修改 base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   title: 'VuePress 文档 by Chaorun',
   description: 'Just playing around',
+  base: '/vuepress-test/dist/',
   dest: 'dist',
   themeConfig: {
     nav: [{ text: 'Home', link: '/' }],
