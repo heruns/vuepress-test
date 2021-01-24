759eaa9fe92df769905c21bc80369cbe48b39e40
heruns <hhccrr@foxmail.com>
2021-01-24 22:36:18 +0800

    项目初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: 'VuePress 文档 by Chaorun',
+  description: 'Just playing around',
+  dest: 'dist',
+  themeConfig: {
+    nav: [{ text: 'Home', link: '/' }],
+    sidebar: ['/']
+  }
+}
